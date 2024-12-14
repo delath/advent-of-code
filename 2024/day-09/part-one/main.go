e1c2fad99ea605abe709be1160d3f240481c9f39
Delath <delath.707@gmail.com>
2024-12-14 23:12:20 +0100

    Solve day 9 part 1 and begin part 2

--- 2024/day-09/part-one/main.go
+++ 2024/day-09/part-one/main.go
@@ -11,7 +11,7 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	var newLine []uint64
+	var newLine []int64
 	var checksum uint64
 	var line string
 
@@ -25,14 +25,14 @@ func main() {
 		if isBlockSize {
 			blockSize := int(line[i] - '0')
 			for j := 0; j < blockSize; j++ {
-				newLine = append(newLine, uint64(blockId))
+				newLine = append(newLine, int64(blockId))
 			}
 			isBlockSize = false
 			blockId++
 		} else {
 			freeSpace := int(line[i] - '0')
 			for j := 0; j < freeSpace; j++ {
-				newLine = append(newLine, uint64('.'))
+				newLine = append(newLine, int64(-1))
 
 			}
 			isBlockSize = true
@@ -41,28 +41,28 @@ func main() {
 
 	isParsed := false
 	for i := 0; i < len(newLine) && !isParsed; i++ {
-		if newLine[i] != uint64('.') {
+		if newLine[i] != int64(-1) {
 			continue
 		}
 		for j := len(newLine) - 1; j >= 0 && !isParsed; j-- {
 			if i >= j {
 				isParsed = true
 			}
-			if newLine[j] == uint64('.') || i >= j {
+			if newLine[j] == int64(-1) || i >= j {
 				continue
 			}
 			newLine[i] = newLine[j]
-			newLine[j] = uint64('.')
+			newLine[j] = int64(-1)
 			break
 		}
 	}
 
 	for i := 0; i < len(newLine); i++ {
-		if newLine[i] == uint64('.') {
+		if newLine[i] == int64(-1) {
 			break
 		}
 
-		checksum += (newLine[i] * uint64(i))
+		checksum += uint64(newLine[i]) * uint64(i)
 	}
 
 	print(checksum)
---
+++ 2024/day-09/part-two/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var newLine []int64
+	var checksum uint64
+	var line string
+
+	for scanner.Scan() {
+		line = scanner.Text()
+	}
+
+	blockId := 0
+	isBlockSize := true
+	for i := 0; i < len(line); i++ {
+		if isBlockSize {
+			blockSize := int(line[i] - '0')
+			for j := 0; j < blockSize; j++ {
+				newLine = append(newLine, int64(blockId))
+			}
+			isBlockSize = false
+			blockId++
+		} else {
+			freeSpace := int(line[i] - '0')
+			for j := 0; j < freeSpace; j++ {
+				newLine = append(newLine, int64(-1))
+
+			}
+			isBlockSize = true
+		}
+	}
+
+	blockId--
+	occurrences := 0
+	for i := len(newLine) - 1; i >= 0 && blockId > 0; i-- {
+		if newLine[i] == int64(blockId) {
+			occurrences++
+		} else if newLine[i] == int64(blockId-1) {
+			freeSpaces := 0
+			for j := 0; j < len(newLine) && j < i; j++ {
+				if newLine[j] != int64(-1) {
+					freeSpaces = 0
+					continue
+				}
+				freeSpaces++
+				if freeSpaces == occurrences {
+					for k := 0; k < freeSpaces; k++ {
+						newLine[j-k] = int64(blockId)
+						newLine[i+1+k] = int64(-1)
+					}
+					break
+				}
+			}
+			occurrences = 1
+			blockId--
+		} else if occurrences == 0 {
+			continue
+		} else {
+			freeSpaces := 0
+			for j := 0; j < len(newLine) && j < i; j++ {
+				if newLine[j] != int64(-1) {
+					freeSpaces = 0
+					continue
+				}
+				freeSpaces++
+				if freeSpaces == occurrences {
+					for k := 0; k < freeSpaces; k++ {
+						newLine[j-k] = int64(blockId)
+						newLine[i+1+k] = int64(-1)
+					}
+					break
+				}
+			}
+			occurrences = 0
+			blockId--
+		}
+		/*for j := len(newLine) - 1; j >= 0 && !isParsed; j-- {
+			if i >= j {
+				isParsed = true
+			}
+			if newLine[j] == int64(-1) || i >= j {
+				continue
+			}
+			newLine[i] = newLine[j]
+			newLine[j] = int64(-1)
+			break
+		}*/
+	}
+
+	for i := 0; i < len(newLine); i++ {
+		if newLine[i] == int64(-1) {
+			continue
+		}
+
+		checksum += uint64(newLine[i]) * uint64(i)
+	}
+
+	print(checksum)
+}
