764dcbac353c1a05a83f874119293c9e7399bb16
Delath <delath.707@gmail.com>
2024-12-14 19:04:21 +0100

    Use uint64 everywhere just for the sake of it

--- 2024/day-09/part-one/main.go
+++ 2024/day-09/part-one/main.go
@@ -15,7 +15,7 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	var newLine []rune
+	var newLine []uint64
 	var checksum uint64
 	var line string
 
@@ -28,14 +28,14 @@ func main() {
 		if i < len(line) {
 			blockSize := int(line[i] - '0')
 			for j := 0; j < blockSize; j++ {
-				newLine = append(newLine, rune(blockId))
+				newLine = append(newLine, uint64(blockId))
 			}
 		}
 
 		if i+1 < len(line) {
 			freeSpace := int(line[i+1] - '0')
 			for j := 0; j < freeSpace; j++ {
-				newLine = append(newLine, '.')
+				newLine = append(newLine, uint64('.'))
 
 			}
 		}
@@ -45,28 +45,28 @@ func main() {
 
 	isParsed := false
 	for i := 0; i < len(newLine) && !isParsed; i++ {
-		if newLine[i] != '.' {
+		if newLine[i] != uint64('.') {
 			continue
 		}
 		for j := len(newLine) - 1; j >= 0 && !isParsed; j-- {
 			if i >= j {
 				isParsed = true
 			}
-			if newLine[j] == '.' || i >= j {
+			if newLine[j] == uint64('.') || i >= j {
 				continue
 			}
 			newLine[i] = newLine[j]
-			newLine[j] = '.'
+			newLine[j] = uint64('.')
 			break
 		}
 	}
 
 	for i := 0; i < len(newLine); i++ {
-		if newLine[i] == '.' {
+		if newLine[i] == uint64('.') {
 			break
 		}
 
-		checksum += (uint64(newLine[i]) * uint64(i))
+		checksum += (newLine[i] * uint64(i))
 	}
 
 	print(checksum)
